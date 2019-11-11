02970d911daa5823786adc15b9b37496875528a6
Max M <46329167+max01110@users.noreply.github.com>
2019-11-11 16:48:50 -0500

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,17 @@
+var a = 600;
+var b = 0
+function setup() {
+    createCanvas(1000, a)
+    background(100, 100, 100);
+//createInput();
+
+
+}
+
+function draw() {
+    textSize(30)
+    text("TEST", width/2, height/2)
+
+
+
+}
